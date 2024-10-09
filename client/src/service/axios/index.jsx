2f2af9a42fa3f7969c38ca8ff2c499fcb133e5e9
Md Shoyeb <96253133+shoyebmd424@users.noreply.github.com>
2024-10-09 19:35:00 +0530

    axios

--- client/src/service/axios/index.jsx
+++ client/src/service/axios/index.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const Axios=axios.create({baseURL:"http://localhost:8000/api/v1/"});
+// export const Server='http://localhost:8000'
+export const Server='http://93.127.163.214:8000'
 
-export const Server='http://localhost:8000'
+const Axios=axios.create({baseURL:`${Server}/api/v1/`});
+
 // const Axios=axios.create({baseURL:"https://inv-plt-j2i7.onrender.com/api/v1/"});
 // export const Server='https://inv-plt-j2i7.onrender.com'
 export default Axios;
