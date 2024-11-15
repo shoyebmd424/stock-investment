81f3fa99fbca25a48e5e9c54aa8886caaac0b329
Md Shoyeb <96253133+shoyebmd424@users.noreply.github.com>
2024-11-15 21:06:14 +0530

    server restart

--- client/src/service/axios/index.jsx
+++ client/src/service/axios/index.jsx
@@ -5,6 +5,6 @@ export const Server='http://93.127.163.214:8000'
 
 const Axios=axios.create({baseURL:`${Server}/api/v1/`});
 
-// const Axios=axios.create({baseURL:"https://inv-plt-j2i7.onrender.com/api/v1/"});
 // export const Server='https://inv-plt-j2i7.onrender.com'
+// const Axios=axios.create({baseURL:"https://inv-plt-j2i7.onrender.com/api/v1/"});
 export default Axios;
