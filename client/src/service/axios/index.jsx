f747d657bd003ae3eb8178bdfeb6f813325af62e
Md Shoyeb <96253133+shoyebmd424@users.noreply.github.com>
2024-11-16 20:08:15 +0530

    crash

--- client/src/service/axios/index.jsx
+++ client/src/service/axios/index.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 export const Server='http://93.127.163.214:8000'
 
 const Axios=axios.create({baseURL:`${Server}/api/v1/`});
-
-// export const Server='https://inv-plt-j2i7.onrender.com'
+// check
 // const Axios=axios.create({baseURL:"https://inv-plt-j2i7.onrender.com/api/v1/"});
+// export const Server='https://inv-plt-j2i7.onrender.com'
 export default Axios;
